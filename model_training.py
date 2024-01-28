a3bb06b9d5f332dd772165c3f7545d7343e34c14
natannvw <natannvw@gmail.com>
2024-01-28 17:40:29 +0200

    Update model_training.py

--- model_training.py
+++ model_training.py
@@ -1,21 +1,28 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 
+
 def train_optimize(X_train, y_train):
     # Example parameter grid
     param_grid = {
-        'n_estimators': [100, 200, 300],
-        'max_depth': [5, 8, 15],
-        'min_samples_split': [2, 5, 10],
-        'min_samples_leaf': [1, 2, 5]
+        "n_estimators": [100, 200, 300],
+        "max_depth": [5, 8, 15],
+        "min_samples_split": [2, 5, 10],
+        "min_samples_leaf": [1, 2, 5],
     }
 
     # Create a RandomForestClassifier
     rf = RandomForestClassifier()
 
     # Setup GridSearchCV
-    grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, 
-                            cv=5, n_jobs=-1, scoring='accuracy', verbose=2)
+    grid_search = GridSearchCV(
+        estimator=rf,
+        param_grid=param_grid,
+        cv=5,
+        n_jobs=-1,
+        scoring="accuracy",
+        verbose=2,
+    )
 
     # Fit grid_search to the data
     grid_search.fit(X_train, y_train)
